287784218cd8993ddb5203a2444d70ff1fc8ba46
siddharth-cse <78216921+siddharth-cse@users.noreply.github.com>
2021-07-15 17:21:05 +0200

    Add files via upload
    added properties

--- read_jsonld.py
+++ read_jsonld.py
@@ -3,9 +3,15 @@ import json
 import os
 import glob
 
+
+#list of IRIs to be added in context 
 iudx='https://voc.iudx.org.in/'    #IUDX IRI
-schema='http://schema.org/'
-geojson='https://purl.org/geojson/vocab#'
+schema='http://schema.org/'		   #Schema.org IRI
+geojson='https://purl.org/geojson/vocab#'   #geojson IRI 
+Fiware='https://uri.fiware.org/ns/data-models#'   #Fiware IRI
+GTFS='http://vocab.gtfs.org/gtfs.ttl#'		#GTFS IRI
+open311 = "http://ontology.eil.utoronto.ca/open311.owl#"  #open311 IRI
+
 
 
 #Default dict elements
@@ -17,11 +23,17 @@ Model_dict={'@graph':[],
 	        "iudx": "https://voc.iudx.org.in/",
 	        "skos": "http://www.w3.org/2004/02/skos/core#",
         	"schema": "http://schema.org/",
-        	"geojson": "https://purl.org/geojson/vocab#"}
+        	"geojson": "https://purl.org/geojson/vocab#",
+        	"Fiware": "https://uri.fiware.org/ns/data-models#",
+        	"GTFS" : "http://vocab.gtfs.org/gtfs.ttl#",
+        	"open311" :"http://ontology.eil.utoronto.ca/open311.owl#"}
 	        }
 
+#Below lists and loops iterate over all classes
 
-Class_folders=[]
+
+
+Class_folders=[]        #list storing all JSONLDs for classes
 
 Class_folders.extend(glob.glob('iudx-voc-master/data-models/*/classes/*'))
 Class_folders.extend(glob.glob('iudx-voc-master/base-schemas/classes/*'))
@@ -31,39 +43,107 @@ Class_folders.extend(glob.glob('iudx-voc-master/data-models/classes/*'))
 
 for iudx_class in Class_folders:
 	myfile = open(iudx_class.replace('\\','/'))
-	data = json.load(myfile)
+	class_data = json.load(myfile)
 	#print((data['@graph'][0]['rdfs:comment']))
 	class_dict={
-		"@id": iudx+(data['@graph'][0]['@id'].split(':')[1]),
+		"@id": iudx+(class_data['@graph'][0]['@id'].split(':')[1]),
 		"@type": 'owl:Class',
-		'comment' : (data['@graph'][0]['rdfs:comment']),
-		'label' : (data['@graph'][0]['rdfs:label']),
+		'comment' : (class_data['@graph'][0]['rdfs:comment']),
+		'label' : (class_data['@graph'][0]['rdfs:label']),
 		'isDefinedBy' : iudx
 		}
 	#To define subclass item inside @graph
-	if('rdfs:subClassOf' in data['@graph'][0]):
-		class_dict['subClassOf'] = {"@id":iudx+data['@graph'][0]['rdfs:subClassOf']['@id'].split(':')[1]}  
+	if('rdfs:subClassOf' in class_data['@graph'][0]):
+		class_dict['subClassOf'] = {"@id":iudx+class_data['@graph'][0]['rdfs:subClassOf']['@id'].split(':')[1]}  
 	
 
 	#To define exactMatch item inside @graph
-	if('skos:exactMatch' in data['@graph'][0]):
-
-		print(data['@graph'][0]['skos:exactMatch'])
+	if('skos:exactMatch' in class_data['@graph'][0]):
 		
 		#Linking schema.org IRI to the matched @id
-		if('schema' in data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
-			class_dict['skos:exactMatch'] = {"@id": schema + data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1]}
+		if('schema' in class_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			class_dict['skos:exactMatch'] = {"@id": schema + class_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1]}
 
 		#Linking geojson IRI to the matched @id
-		if('geojson' in data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
-			class_dict['skos:exactMatch'] = {"@id": geojson + data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1]}
+		if('geojson' in class_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			class_dict['skos:exactMatch'] = {"@id": geojson + class_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1]}
 
 
-	if('skos:closeMatch' in data['@graph'][0]):
-		print(data['@graph'][0]['skos:closeMatch'])
-
+	if('skos:closeMatch' in class_data['@graph'][0]):
+		if('schema' in class_data['@graph'][0]['skos:closeMatch']['@id'].split(':')[0]):
+			class_dict['skos:closeMatch'] = {"@id": schema + class_data['@graph'][0]['skos:closeMatch']['@id'].split(':')[1]}
+	
 
 	Model_dict['@graph'].append(class_dict)
+
+
+
+#Below lists and loops iterate over all classes
+
+Properties_folders=[]     #list storing all JSONLDs for properties
+
+Properties_folders.extend(glob.glob('iudx-voc-master/data-models/*/properties/*'))
+Properties_folders.extend(glob.glob('iudx-voc-master/base-schemas/properties/*'))
+
+
+for iudx_properties in Properties_folders:
+	myfile = open(iudx_properties.replace('\\','/'))
+	properties_data = json.load(myfile)
+	properties_dict={
+		"@id": iudx+(properties_data['@graph'][0]['@id'].split(':')[1]),
+		"@type": properties_data['@graph'][0]['@type'],
+		'comment' : (properties_data['@graph'][0]['rdfs:comment']),
+		'label' : (properties_data['@graph'][0]['rdfs:label']),
+		'isDefinedBy' : iudx,
+		'iudx:domainIncludes' : properties_data['@graph'][0]['iudx:domainIncludes'],
+		'iudx:rangeIncludes' : properties_data['@graph'][0]['iudx:rangeIncludes']
+		}
+
+	#To define exactMatch item inside @graph
+	if('skos:exactMatch' in properties_data['@graph'][0]):
+		#print(properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0],iudx_properties)
+
+
+		#Linking Fiware IRI to the matched @id
+		if(('fiware') in properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0] or('Fiware') in properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			properties_dict['skos:exactMatch'] = {"@id": Fiware + properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1].strip()}
+			#print(properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0],iudx_properties)
+
+		#Linking schema IRI to the matched @id
+		if(('schema') in properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			properties_dict['skos:exactMatch'] = {"@id": schema + properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[1].strip()}
+			#print(properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0],iudx_properties)
+
+
+		#Linking GTFS IRI to the matched @id
+		if(('GTFS') in properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			properties_dict['skos:exactMatch'] = {"@id": GTFS + properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[-1].strip()}
+			#print(properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0],iudx_properties)
+
+
+		#Linking open311 IRI to the matched @id
+		if(('open311') in properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0]):
+			#print(properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[0],iudx_properties)			
+			properties_dict['skos:exactMatch'] = {"@id": open311 + properties_data['@graph'][0]['skos:exactMatch']['@id'].split(':')[-1].strip()}
+		
+
+
+
+
+
+
+
+	#if('iudx:domainIncludes' in properties_data['@graph'][0]):
+	#	print(properties_data['@graph'][0]['iudx:domainIncludes'])
+
+
+
+	Model_dict['@graph'].append(properties_dict)
+
+
+
+
+
 
 
 
